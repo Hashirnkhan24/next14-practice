538a304cc0efeae6fc7d500217f1d90d28d8bf05
Hashirkhan24 <khn24202@gmail.com>
2024-02-20 12:43:07 +0530

    Create loading, error and not found layouts.

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const Links = () => {
+    const links = [
+        {
+            title: "Home",
+            path: "/",
+        },
+        {
+            title: "About",
+            path: "/about",
+        },
+        {
+            title: "Contact",
+            path: "/contact",
+        },
+        {
+            title: "Blog",
+            path: "/blog",
+        },
+    ]
+    return (
+        links.map((link) => <Link href={link.path} key={link.title}>{link.title}</Link>)
+    )
+};
+
+export default Links;
